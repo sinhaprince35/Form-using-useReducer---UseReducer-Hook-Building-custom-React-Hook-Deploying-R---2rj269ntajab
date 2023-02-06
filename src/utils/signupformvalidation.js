2a22bdc10ccbb8c5ab814cb239d07892b20733e0
Prince Kumar <112944356+sinhaprince35@users.noreply.github.com>
2023-02-06 16:54:13 +0530

    Update signupformvalidation.js

--- src/utils/signupformvalidation.js
+++ src/utils/signupformvalidation.js
@@ -1,38 +1,31 @@
-const signUpFormReducer = (state, action) => {
-    switch (action.type) {
-        case 'input':
-            return {
-                ...state,
-                input:{
-                    ...state.input,
-                    [action.payload.field]: action.payload.value
-                }      
-            }
-        case 'error':
-            return {
-                ...state,
-                errors:{
-                    ...state.errors,
-                    [action.payload.field]: action.payload.value
-                }      
-            }
-        case 'resetErrors':
-            const errors = {}
-            for(const t in state.errors){
-                errors[t] = ""
-            }
-            return {
-                ...state,
-                errors
-            }
-        case 'toggleConsent':
-            return {
-                ...state,
-                consent: !state.consent
-            }
-        default:
-            break;
-    }
+const pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/; 
+
+function isEmailAddress(str) {
+
+    return str.match(pattern);    
+
 }
 
-export { signUpFormReducer }
+const signUpFormValidation = (formData) => {
+    const error = {}
+    const fields = ['email','password','name']
+    fields.forEach(field=>{
+        if(!formData[`${field}`]){
+            error[[field]] = `${field} not present`
+        }
+    })
+    if(formData.name && (formData.name.length < 3 || formData.name.length > 20)){
+        error['name'] = 'Name should be atleast 4 chars and less than 20 chars'
+    }
+    if(formData.password && (formData.password.length < 6  || formData.password.length > 30)){
+        error['password'] = 'Password should be atleast 6 chars and less than 30 chars'
+    }
+
+    if(formData.email && isEmailAddress(formData.email) === null){
+        error['email'] = 'Not a valid email'
+    }
+    if(Object.keys(error).length === 0) return null
+    return error
+}
+
+export { signUpFormValidation }
